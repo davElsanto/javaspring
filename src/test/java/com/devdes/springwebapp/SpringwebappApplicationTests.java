717013210f3e9c07ff42777c7f476bce1fd0d867
David Santos <insa1988@gmail.com>
2021-02-24 22:27:24 -0500

    first commit

---
+++ src/test/java/com/devdes/springwebapp/SpringwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdes.springwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
